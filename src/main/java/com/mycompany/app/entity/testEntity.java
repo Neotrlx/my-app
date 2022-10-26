c05d469fb095e35c5a826a41c749baa84ea27bd3
Neotrlx <luis.e.sanmartin.e@gmail.com>
2022-10-26 17:04:46 -0500

    Nombre del Proyecto: my-app. Descripcion: Version final del proyecto my-app

---
+++ src/main/java/com/mycompany/app/entity/testEntity.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.mycompany.app.entity;
+
+/**
+ * @author luis_
+ *
+ */
+public class testEntity {
+	/**
+	 * Propiedad de prueba para la muestra de compilacion del archivo testentity.
+	 */
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
